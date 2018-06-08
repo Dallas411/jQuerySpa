21b4657b1bd5eea0a0274d692a3c78a227da765c
Marco Dalla Libera <marco@dallalibera.net>
2018-06-08 18:21:52 +0200

    Aggiungere i file di progetto.

---
+++ jQuerySpa/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using jQuerySpa.Models;
+
+namespace jQuerySpa.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string postText)
+        {
+            return RedirectToAction("Table", "Home", new { postText });
+        }
+
+        public ActionResult Table(string getText, string postText, string mySubmit)
+        {
+            ViewBag.Message = "Renders dataTable";
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ jQuerySpa/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace jQuerySpa.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
